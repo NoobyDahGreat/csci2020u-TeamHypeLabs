8edec94f0336fd3c2c37300a8601ab9d7f2718b1
Jude Arokiam <itsjude96@gmail.com>
2016-02-08 13:18:30 -0500

    testing javaFX project

---
+++ lab04/src/sample/Application.java
@@ -0,0 +1,14 @@
+package sample;
+
+/**
+ * Created by jude on 08/02/16.
+ */
+public class Application {
+    private String username;
+    private String password;
+    private String fullName;
+    private String emailAddress;
+    private String number;
+
+    
+}
