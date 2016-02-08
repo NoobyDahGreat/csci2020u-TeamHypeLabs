50191b04eab5c5ab55d3c4d63fcc5ae319c65cff
Jude Arokiam <itsjude96@gmail.com>
2016-02-08 15:41:21 -0500

    Added all remainging text fields and date picker

--- lab04/src/sample/Main.java
+++ lab04/src/sample/Main.java
@@ -1,6 +1,8 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
@@ -8,6 +10,7 @@ import javafx.stage.Stage;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
 
+import java.time.LocalDate;
 
 
 public class Main extends Application {
@@ -24,22 +27,53 @@ public class Main extends Application {
         editArea.setVgap(10);
         editArea.setHgap(10);
 
-        Label nameLabel = new Label("Full name:");
-        editArea.add(nameLabel, 0, 1);
-        TextField nameField = new TextField();
-        nameField.setPromptText("Full Name");
-        editArea.add(nameField, 1, 1);
+        Label userNameLabel = new Label("Username:");
+        editArea.add(userNameLabel, 0, 1);
+        TextField userNameField = new TextField();
+        userNameField.setPromptText("Username");
+        editArea.add(userNameField, 1, 1);
 
-        Label passLabel = new Label("Password:");
-        editArea.add(passLabel, 0, 2);
-        PasswordField passField = new PasswordField();
-        passField.setPromptText("Password");
-        editArea.add(passField, 1, 2);
+        Label passwordLabel = new Label("Password:");
+        editArea.add(passwordLabel, 0, 2);
+        PasswordField passwordField = new PasswordField();
+        passwordField.setPromptText("Password");
+        editArea.add(passwordField, 1, 2);
+
+        Label fullNameLabel = new Label("Full name:");
+        editArea.add(fullNameLabel, 0, 3);
+        TextField fullNameField = new TextField();
+        fullNameField.setPromptText("Full Name");
+        editArea.add(fullNameField, 1, 3);
+
+        Label emailLabel = new Label("E-Mail:");
+        editArea.add(emailLabel, 0, 4);
+        TextField emailField = new TextField();
+        emailField.setPromptText("email@stuff.com");
+        editArea.add(emailField, 1, 4);
+
+        Label phoneLabel = new Label("Phone #:");
+        editArea.add(phoneLabel, 0, 5);
+        TextField phoneField = new TextField();
+        phoneField.setPromptText("000-000-0000");
+        editArea.add(phoneField, 1, 5);
+
+        Label dateLabel = new Label("Date of Birth:");
+        editArea.add(dateLabel, 0, 6);
+        final DatePicker datePicker = new DatePicker();
+        datePicker.setOnAction(new EventHandler() {
+            public void handle(Event t) {
+                LocalDate date = datePicker.getValue();
+                System.err.println("Selected date: " + date);
+            }
+        });
+        editArea.add(datePicker, 1, 6);
+
+
 
         layout = new BorderPane();
         layout.setCenter(editArea);
 
-        primaryStage.setScene(new Scene(layout, 300, 275));
+        primaryStage.setScene(new Scene(layout, 325, 300));
         primaryStage.show();
     }
 
