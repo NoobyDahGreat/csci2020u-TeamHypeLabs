71250ecee2b5ea16369dda600ed942b1cbecf12d
Andrei Stoica <andrei.stoica@uoit.net>
2016-02-08 14:11:17 -0500

    Added buttons for Name and password

--- lab04/src/sample/Main.java
+++ lab04/src/sample/Main.java
@@ -2,17 +2,53 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.*;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.image.*;
+import javafx.collections.*;
+import javafx.event.*;
+import javafx.scene.control.TableColumn.CellEditEvent;
+import javafx.scene.control.cell.*;
+
 
 public class Main extends Application {
+    private BorderPane layout;
+    private TextField nameField, username, email, phone, password;
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Lab 04 Solution");
+
+        GridPane editArea = new GridPane();
+        editArea.setPadding(new Insets(10, 10, 10, 10));
+        editArea.setVgap(10);
+        editArea.setHgap(10);
+
+        Label nameLabel = new Label("Full name:");
+        editArea.add(nameLabel, 0, 1);
+        TextField nameField = new TextField();
+        nameField.setPromptText("Full Name");
+        editArea.add(nameField, 1, 1);
+
+        Label passLabel = new Label("Password:");
+        editArea.add(passLabel, 0, 2);
+        PasswordField passField = new PasswordField();
+        passField.setPromptText("Password");
+        editArea.add(passField, 1, 2);
+
+        layout = new BorderPane();
+        layout.setCenter(editArea);
+
+        primaryStage.setScene(new Scene(layout, 300, 275));
         primaryStage.show();
     }
 
