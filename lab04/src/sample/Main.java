0c58347902a8d4cb5990e0c75c13a218dc40039f
Brad Gardiner <bradgardiner21@gmail.com>
2016-02-08 13:38:44 -0500

    Testing version cntrol

--- lab04/src/sample/Main.java
+++ lab04/src/sample/Main.java
@@ -4,14 +4,24 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.scene.input.*;
+import javafx.scene.image.*;
+
+import java.awt.*;
+
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Register");
+
+        TextField
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
