676ae2951a940f0ab54c54c4b7129d12157f2b55
Jude Arokiam <itsjude96@gmail.com>
2016-02-29 13:18:01 -0500

    added template to lab 6 project and editied gitignore

--- lab06/src/sample/Main.java
+++ lab06/src/sample/Main.java
@@ -1,23 +1,54 @@
 package sample;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Main extends Application {
+    @FXML
+    private Canvas canvas;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Group root = new Group();
+        Scene scene = new Scene(root, 30000, 37000, Color.LIGHTGRAY);
+
+        canvas = new Canvas();
+        canvas.widthProperty().bind(primaryStage.widthProperty());
+        canvas.heightProperty().bind(primaryStage.heightProperty());
+
+        root.getChildren().add(canvas);
+        primaryStage.setScene(scene);
         primaryStage.show();
+
+        draw(root);
+    }
+
+    private void draw(Group group) {
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        System.out.println("width: " + canvas.getWidth());
+        System.out.println("height: " + canvas.getHeight());
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
+    public static void main(String[] args) {launch(args);
     }
 }
