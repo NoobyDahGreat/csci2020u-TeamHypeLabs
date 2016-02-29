a5a62f3fe401301db95e4c265226a98c6f54fec4
Jude Arokiam <itsjude96@gmail.com>
2016-02-29 13:40:43 -0500

    added data for bar graph

--- lab06/src/sample/Main.java
+++ lab06/src/sample/Main.java
@@ -22,6 +22,16 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class Main extends Application {
+    private static double[] avgHousePricesByYear = {
+            247381.0,264171.4,287715.3,294736.1,
+            308431.4,322635.9,340253.0,363153.7
+    };
+    private static double[] avgCommercialPricesByYear = {
+            1121585.3,1219479.5,1246354.2,1295364.8,
+            1335932.6,1472362.0,1583521.9,1613246.3
+    };
+
+
     @FXML
     private Canvas canvas;
 
