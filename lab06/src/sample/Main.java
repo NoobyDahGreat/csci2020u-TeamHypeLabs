7a9c0af8920eb6cf7899b66f5ad05b1f12d309ca
Jude Arokiam <itsjude96@gmail.com>
2016-02-29 14:01:34 -0500

    added bars for bargraph

--- lab06/src/sample/Main.java
+++ lab06/src/sample/Main.java
@@ -12,6 +12,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
@@ -44,21 +48,43 @@ public class Main extends Application {
 
     @FXML
     private Canvas canvas;
-
+        final static String bar1 = "1";
+        final static String bar2 = "2";
+        final static String bar3 = "3";
+        final static String bar4 = "4";
+        final static String bar5 = "5";
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Group root = new Group();
-        Scene scene = new Scene(root, 30000, 37000, Color.LIGHTGRAY);
+    public void start(Stage stage) throws Exception{
+        stage.setTitle("Bar Chart Sample");
+        final CategoryAxis xAxis = new CategoryAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        final BarChart<String,Number> bc =
+                new BarChart<String,Number>(xAxis,yAxis);
+        bc.setTitle("Country Summary");
+        xAxis.setLabel("Country");
+        yAxis.setLabel("Value");
 
-        canvas = new Canvas();
-        canvas.widthProperty().bind(primaryStage.widthProperty());
-        canvas.heightProperty().bind(primaryStage.heightProperty());
+        XYChart.Series series1 = new XYChart.Series();
+        series1.setName("2003");
+        series1.getData().add(new XYChart.Data(bar1, avgHousePricesByYear[0]));
+        series1.getData().add(new XYChart.Data(bar2, avgHousePricesByYear[1]));
+        series1.getData().add(new XYChart.Data(bar3, avgHousePricesByYear[2]));
+        series1.getData().add(new XYChart.Data(bar4, avgHousePricesByYear[3]));
+        series1.getData().add(new XYChart.Data(bar5, avgHousePricesByYear[4]));
 
-        root.getChildren().add(canvas);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        XYChart.Series series2 = new XYChart.Series();
+        series2.setName("2004");
+        series2.getData().add(new XYChart.Data(bar1, 57401.85));
+        series2.getData().add(new XYChart.Data(bar2, 41941.19));
+        series2.getData().add(new XYChart.Data(bar3, 45263.37));
+        series2.getData().add(new XYChart.Data(bar4, 117320.16));
+        series2.getData().add(new XYChart.Data(bar5, 14845.27));
 
-        draw(root);
+
+        Scene scene  = new Scene(bc,800,600);
+        bc.getData().addAll(series1, series2);
+        stage.setScene(scene);
+        stage.show();
     }
 
     private void draw(Group group) {
