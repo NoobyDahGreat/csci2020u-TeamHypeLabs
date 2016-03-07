81a9dd73c3cbd47c2208359bf3618de432a14c1b
Brad Gardiner <bradgardiner21@gmail.com>
2016-03-07 13:09:12 -0500

    reading CSV file

--- lab07/src/sample/Main.java
+++ lab07/src/sample/Main.java
@@ -5,6 +5,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 
 public class Main extends Application {
 
@@ -19,6 +23,19 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+        try {
+            Scanner scanner = new Scanner(new File("/home/brad/Documents/csci2020u/lab07/weatherwarnings-2015.csv"));
+            scanner.useDelimiter(",");
+            while (scanner.hasNext()) {
+                /////ADD CODE HERE
+            }
+            scanner.close();
+        }
+
+        catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+
 
     }
 }
