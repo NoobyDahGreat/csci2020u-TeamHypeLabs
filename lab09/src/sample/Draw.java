fa404f1fef06d6829f78c4addf8842873e54187f
Andrei Stoica <andrei.stoica@uoit.net>
2016-03-21 13:02:23 -0400

    added Draw class

---
+++ lab09/src/sample/Draw.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.collections.ObservableList;
+
+/**
+ * Created by andrei on 21/03/16.
+ */
+public class Draw {
+    public static void drawLinePlot(ObservableList<Double> firstList, ObservableList<Double> secondList) {
+
+    }
+
+    public static void plotLine(ObservableList<Double> list) {
+
+    }
+
+}
